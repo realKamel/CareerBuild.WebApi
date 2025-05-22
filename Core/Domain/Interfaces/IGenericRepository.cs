6b8b5f7e32f155244f5f0486b8412e0d0ac9433b
abdelrahman <abdo.aly.k.m@gmail.com>
2025-05-22 15:56:38 +0300

    Implement generic repository and unit of work pattern
    This commit introduces a generic repository pattern by adding the `IGenericRepository` and `IUnitOfWork` interfaces,

---
+++ Core/Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+	public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+	{
+		Task AddAsync(TEntity entity);
+		void Update(TEntity entity);
+		void Remove(TEntity entity);
+		Task<TEntity?> GetByIdAsync(TKey id);
+		Task<IEnumerable<TEntity>> GetAllAsync();
+	}
+
+}
---
+++ Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Persistence.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+	public class UnitOfWork(AppDbContext _dbContext) : IUnitOfWork
+	{
+		//Dictionary for the created Repos
+		private Dictionary<string, object> _repositories = [];
+		public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+		{
+			//get type name 
+			var typeName = typeof(TEntity).Name;
+			if (_repositories.TryGetValue(typeName, out object? value))
+			{
+				return value as IGenericRepository<TEntity, TKey>;
+			}
+			else
+			{
+				//1. Create object 
+				var newTypeOjbect = new GenericRepository<TEntity, TKey>(_dbContext);
+				//2. Store in Dictionary
+				_repositories[typeof(TEntity).Name] = newTypeOjbect;
+				//3. return the new object
+				return newTypeOjbect;
+			}
+		}
+
+		public async Task<int> SaveChangesAsync()
+		{
+			return await _dbContext.SaveChangesAsync();
+		}
+	}
+}
