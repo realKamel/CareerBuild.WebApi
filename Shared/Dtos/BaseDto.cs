09756ee72da27b1b5aa56f356f00eb7952382eb9
abdelrahman <abdo.aly.k.m@gmail.com>
2025-05-28 21:08:16 +0300

    Refactor BaseDto.cs and update property attributes
    - Changed namespace to `Shared.Dtos`.
    - Updated `MaxLength` attributes for `Name` and `Description` properties to include spaces.
    - Added a new `DateTime` property.
    - Moved `UpdatedAt` property to follow the new `DateTime` property.

--- Shared/Dtos/BaseDto.cs
+++ Shared/Dtos/BaseDto.cs
@@ -6,11 +6,12 @@ public class BaseDto
 {
 	public int Id { get; set; }
 
-	[MaxLength(250, ErrorMessage = "Maximum length is {250}")]
+	[MaxLength( 250, ErrorMessage = "Maximum length is {250}" )]
 	public string Name { get; set; } = null!; // required
 
-	[MaxLength(1000, ErrorMessage = "Maximum length is {1000}")]
+	[MaxLength( 1000, ErrorMessage = "Maximum length is {1000}" )]
 	public string? Description { get; set; } = null; // required
 
-	public DateTime? UpdatedAt { get; set; } 
+	public DateTime DateTime { get; set; }
+	public DateTime? UpdatedAt { get; set; }
 }
